f90e1116ac5e5658d06a799d1c2348e986442760
System Administrator <root@Sabrinas-MacBook-Pro-4.local>
2022-07-06 20:34:44 -0400

    Completed exersice

--- count-pairs.js
+++ count-pairs.js
@@ -1,79 +1,23 @@
 // add whatever parameters you deem necessary
 function countPairs(arr, sum) {
-    let count = 0;
+    arr.sort((a,b) => a-b);
     let left = 0;
-    let right = arr.length -1;
+    let right = arr.length - 1;
+    let count = 0;
 
-    while(right !== -1) {
-        for(let i = 0; i < arr.length; i++) {
-            console.log("sum:", arr[left] + arr[i], "left:", left, "i:", i)
-
-            if(right === 0) {
-                right--;
-            }
-            else if(arr[right] + arr[i] === sum) {
-                count++
-                right--
-            }
-            else {
-                right--
-            }
+    while(left < right) {
+        let findSum = (arr[left] + arr[right]);
+        if(findSum === sum) {
+            count++
+            left++
+            right++
+        }
+        else if(findSum < sum) {
+            left++;
+        }
+        else {
+            right--;
         }
     }
-        return count;
+    return count;
 }
-
-    // let left = 0;
-    // let right = arr.length - 1;
-    // let count = 0;
-
-    // while(left < right) {
-    //     let findSum = (arr[left] + arr[right]);
-    //     console.log(findSum)
-    //     if(findSum === sum) {
-    //         console.log(count, findSum, sum)
-    //         count++
-    //         left++
-    //     }
-    //     else if(findSum < sum) {
-    //         left++;
-    //     }
-    //     else {
-    //         right--;
-    //     }
-    // }
-
-    // left = 0
-    // right = arr.length - 1;
-    // while(right > left) {
-    //     let findSum = (arr[left] + arr[right]);
-    //     console.log(findSum)
-    //     if(findSum === sum) {
-    //         console.log(count, findSum, sum)
-    //         count++
-    //         right--
-    //     }
-    //     else {
-    //         right--;
-    //     }
-    // }
-    // console.log(count)
-    // return count;
-
-    // left = 0
-    // right = 1;
-    // for(let i = 0; i < arr.length; i++) {
-    //     let findSum = (arr[left] + arr[right]);
-    //     console.log(count, findSum)
-    //     if(findSum === sum) {
-    //         count++
-    //         right++
-    //         left++;
-    //     }
-    //     else {
-    //         left++;
-    //         right++;
-    //     }
-    // }
-
-// }
--- separate-positive.js
+++ separate-positive.js
@@ -1,20 +1,21 @@
 // add whatever parameters you deem necessary
 function separatePositive(arr) {
 
-    let midIdx = Math.floor((arr.length - 1) /2);
-    let left = midIdx;
-    let right = midIdx + 1;
-
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr.length; j++) {
-            if(arr[j] < arr[j+1]) {
-                let temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-            }
-        }
+    let start = 0;
+    let end = arr.length - 1;
+  
+    while (start < end) {
+      if (arr[start] < 0 && arr[end] > 0) {
+        // ES2015
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+  
+        start += 1;
+        end -= 1;
+      } else {
+        if (arr[start] > 0) start += 1;
+        else end -= 1;
+      }
     }
-   
     return arr;
 }
 
